a771727d351e5d65498558004f3476336dae5e2c
iesipsstudent <55012823+iesipsstudent@users.noreply.github.com>
2019-09-09 12:05:35 +0530

    Update DBConnector.java

--- src/java/db/DBConnector.java
+++ src/java/db/DBConnector.java
@@ -4,11 +4,6 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
-
-/**
- *
- * @author Pankaj
- */
 public class DBConnector 
 {
     static Connection con=null;
