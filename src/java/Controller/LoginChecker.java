75bf9cd5be010634e623a4fd7bc2143ac1e71b6e
Atisha759 <55011898+Atisha759@users.noreply.github.com>
2019-09-09 11:18:54 +0530

    Update LoginChecker.java

--- src/java/Controller/LoginChecker.java
+++ src/java/Controller/LoginChecker.java
@@ -9,10 +9,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import model.LoginAuthenticator;
 
-/**
- *
- * @author Pankaj
- */
+
 public class LoginChecker extends HttpServlet 
 {
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
