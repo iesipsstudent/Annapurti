792ac8b46c7a3437438a665385a0db5bb785dca8
iesipsstudent <55012823+iesipsstudent@users.noreply.github.com>
2019-09-09 12:03:26 +0530

    Update Main.java

--- src/java/model/Main.java
+++ src/java/model/Main.java
@@ -10,11 +10,6 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-
-/**
- *
- * @author Alok Hirwe
- */
 public class Main {
 
     private UserAccount user;
